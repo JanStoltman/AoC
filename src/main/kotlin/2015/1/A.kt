3ace8e675ac9d295858b0764b3a936c4d8228685
janstoltman <stoltmanjan@gmail.com>
2020-12-05 17:57:58 +0200

    2015.1 solution

--- src/main/kotlin/2015/1/A.kt
+++ src/main/kotlin/2015/1/A.kt
@@ -1,5 +1,7 @@
 package `2015`.`1`
 
+import java.io.File
+
 /**
 Santa is trying to deliver presents in a large apartment building, but he can't find the right floor -
 the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions
@@ -16,7 +18,26 @@ For example:
 To what floor do the instructions take Santa?
  **/
 
-
 fun main() {
+    assert(algorithm("(())") == 0)
+    assert(algorithm("()()") == 0)
+    assert(algorithm("(((") == 3)
+    assert(algorithm("(()(()(") == 3)
+    assert(algorithm("))(((((") == 3)
+    assert(algorithm("())") == -1)
+    assert(algorithm("))") == -1)
+    assert(algorithm(")))") == -3)
+    assert(algorithm(")())())") == -1)
 
+    val input = File("src/main/resources/2015_1").readText()
+    println(algorithm(input))
 }
+
+private fun algorithm(input: String) =
+    input.sumBy { c ->
+        when (c) {
+            '(' -> 1
+            ')' -> -1
+            else -> 0
+        }
+    }
